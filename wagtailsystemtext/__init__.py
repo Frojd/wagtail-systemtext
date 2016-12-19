c103efaf693eadb90c165ed49564c4f3dfbfd570
Martin Sandström <martin@marteinn.se>
2016-12-19 21:05:45 +0100

    Added package metadata

---
+++ wagtailsystemtext/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+wagtailsystemtext
+----------
+
+"""
+
+__title__ = 'wagtailsystemtext'
+__version__ = '1.0.0'
+__build__ = 100
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2016 Martin Sandström"
+
+default_app_config = 'wagtailsystemtext.apps.WagtailSystemTextConfig'
