ad3e52eb611b5eb950407910b7df331fc0199cc9
Martin Sandström <martin@marteinn.se>
2016-12-20 22:19:30 +0100

    Bumped version

--- wagtailsystemtext/__init__.py
+++ wagtailsystemtext/__init__.py
@@ -4,12 +4,12 @@
 """
 wagtailsystemtext
 ----------
-
+Simplified Wagtail system text management
 """
 
 __title__ = 'wagtailsystemtext'
-__version__ = '1.0.1'
-__build__ = 101
+__version__ = '1.0.2'
+__build__ = 102
 __author__ = "Martin Sandström"
 __license__ = "MIT"
 __copyright__ = "Copyright 2016 Martin Sandström"
