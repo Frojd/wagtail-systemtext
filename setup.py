151158fea716f1fe1f3d79980897358f7b126be5
Martin Sandström <martin@marteinn.se>
2019-01-30 07:33:47 +0100

    Drop python 2 and wagtail -1 from setup

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'requests',
-        'wagtail>=1.7'
+        'wagtail>=2.0'
     ],
     license="MIT",
     zip_safe=False,
@@ -51,8 +51,6 @@ setup(
         'Intended Audience :: Developers',
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        'Programming Language :: Python :: 2',
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
